3b0eef41b0ef134483e36b6743ba6a20a1656243
Samir Moreira Antonio <samir.ma@gmail.com>
2015-12-18 00:36:46 -0200

    database with hidden post and commentary wifi_locations

--- sql/mod_post.sql
+++ sql/mod_post.sql
@@ -36,4 +36,47 @@ CREATE TABLE rates
   username text NOT NULL,
   rate integer NOT NULL DEFAULT 0,
   created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+
+CREATE TABLE hidden_posts
+(
+  id serial,
+  post_id integer,
+  comment_id integer,
+  hidden_from_user text NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
+); 
+
+CREATE TABLE hidden_comments
+(
+  id serial,
+  comment_id integer,
+  hidden_from_user text NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
+); 
+
+CREATE TABLE wifi_locations
+(
+  id serial,
+  wifi_name text,
+  location geometry,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
+); 
+
+
+CREATE TABLE wifi_locations
+(
+  id serial,
+  wifi_name text,
+  location geometry,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
+); 
+
+
+CREATE TABLE wifi_posts
+(
+  wifi_locations_id integer,
+  post_id integer,
+  created_at TIMESTAMP NOT NULL DEFAULT now()
 ); 
